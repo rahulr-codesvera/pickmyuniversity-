272c58235b94fb94e9c8900c7b4c31862a5f2eb1
rahulr-codesvera <rahulrajmk@codesvera.com>
2024-12-09 18:22:55 +0530

    changed navbar a to link

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
               <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
                   <NavigationMenuLink asChild>
-                    <a
+                    <Link
                       className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                       href="/"
                     >
@@ -59,7 +59,7 @@ const Navbar = () => {
                         paste into your apps. Accessible. Customizable. Open
                         Source.
                       </p>
-                    </a>
+                    </Link>
                   </NavigationMenuLink>
                 </li>
                 <h2>link 1</h2>
