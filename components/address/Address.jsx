ffed119a2e987d5c18b32a7304c796201534d1d2
rahulr-codesvera <rahulrajmk@codesvera.com>
2024-12-11 18:02:07 +0530

    completed address section

--- components/address/Address.jsx
+++ components/address/Address.jsx
@@ -3,6 +3,29 @@ import styles from "./Address.module.css";
 import Image from "next/image";
 
 const Address = () => {
+  const addrSpanList = [
+    {
+      id: 1,
+      label: "Address",
+      address: ` N0-317, VPM Plaza, 2nd floor, 80ft road, \n
+                Anna Nagar, Madurai, Tamil Nadu.`,
+    },
+    {
+      id: 2,
+      label: "Landmark",
+      address: "Near HDFC Bank",
+    },
+    {
+      id: 3,
+      label: "Pincode",
+      address: "625020, Tamil Nadu",
+    },
+    {
+      id: 4,
+      label: "Mobile",
+      address: "+91 9384374741\n+91 9994058803",
+    },
+  ];
   return (
     <div className={styles.container}>
       <div className={styles.addrContainer}>
@@ -16,7 +39,25 @@ const Address = () => {
             />
           </div>
         </div>
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+          <h5 className={styles.heading}>Head Office</h5>
+          <p className={styles.name}>Kaushik</p>
+          <div className={styles.addrBox}>
+            {addrSpanList.map((item) => (
+              <span key={item.id} className={styles.addrSpan}>
+                <p className={styles.label}>{item.label}</p>
+                <p>:</p>
+
+                {item.address.split("\n").map((item, index) => (
+                  <React.Fragment key={index}>
+                    {item}
+                    <br />
+                  </React.Fragment>
+                ))}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
